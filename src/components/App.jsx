9895da2a6e2a81f48b70b99d650ea71492da00d3
blavikensbutcher <luckertheory@gmail.com>
2024-01-30 21:02:07 +0200

    add useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,16 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ContactList } from './ContactList/ContactList.jsx';
 import { SearchBox } from './SearchBox/SearchBox.jsx';
 import { ContactForm } from './ContactForm/ContactForm.jsx';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
-  const [contacts, setContact] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContact] = useState(JSON.parse(localStorage.getItem('contacts')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts, setContact]);
 
   function deleteUser(userID) {
     setContact(prevState => {
